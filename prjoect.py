ece53c061d11421d75c3d763b3244873cd3604fc
mariz92 <103050971+mariz92@users.noreply.github.com>
2022-04-05 16:28:05 +0200

    Update prjoect.py

--- prjoect.py
+++ prjoect.py
@@ -9,4 +9,22 @@ def organize_data():
     fields = csv.reader(csvfile, delimiter=';')
     for field in fields:
         print(field[3], field[4], field[5])
-                
+     
+    
+    # haversine
+from math import radians, cos, sin, asin, sqrt
+def haversine(lon1, lat1, lon2, lat2):
+    """
+    Calculate the great circle distance in kilometers between two points 
+    on the earth (specified in decimal degrees)
+    """
+    # convert decimal degrees to radians 
+    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+
+    # haversine formula 
+    dlon = lon2 - lon1 
+    dlat = lat2 - lat1 
+    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
+    c = 2 * asin(sqrt(a)) 
+    r = 6371 # Radius of earth in kilometers. Use 3956 for miles. Determines return value units.
+    return c * r
