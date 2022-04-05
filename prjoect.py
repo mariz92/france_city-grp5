92f8e34c8fafeac9521e6352cf03b1c9c47c055c
mariz92 <103050971+mariz92@users.noreply.github.com>
2022-04-05 16:27:35 +0200

    Update prjoect.py

--- prjoect.py
+++ prjoect.py
@@ -1,1 +1,12 @@
 ello = "ello"
+# read csv file
+import csv
+                    
+# organize data
+def organize_data():
+    fName = 'laposte_hexasmal.small.csv'
+    with open(fName, newline='') as csvfile:
+    fields = csv.reader(csvfile, delimiter=';')
+    for field in fields:
+        print(field[3], field[4], field[5])
+                
